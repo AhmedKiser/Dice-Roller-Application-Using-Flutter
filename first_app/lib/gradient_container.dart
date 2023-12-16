b497126c5ff8d2d31f16b6c20a686a82cbba6222
Ahmed Kiser <ahmed.kiser@northsouth.edu>
2023-12-16 22:56:50 +0600

    variable added

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
 class GradinetContainer extends StatelessWidget{
 
   const GradinetContainer({super.key});
+  
+
   @override
   Widget build(context) {
+    
     return Container(
-          decoration: const BoxDecoration(
+          decoration:  BoxDecoration(
             gradient: LinearGradient(
-              colors: [Colors.deepPurple,Colors.lightBlue],
-              begin: Alignment.topLeft,
+              colors:const [Colors.deepPurple,Colors.lightBlue],
+              begin: startAlignment,
               end: Alignment.bottomRight
             )
           ),
